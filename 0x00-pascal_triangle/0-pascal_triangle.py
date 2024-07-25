fcef05ad75f1c2df871b6ac9193eca50d85009bc
Mohammed <mrcloud.med@gmail.com>
2024-07-25 23:32:01 +0100

    Pascal Trianlge

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    triangle = []
+    
+    for i in range(n):
+        row = [1] * (i + 1)  # Start each row with 1s
+        
+        # Update values based on the previous row
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        
+        triangle.append(row)
+    
+    return triangle
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository for ALX Interiew preparation
