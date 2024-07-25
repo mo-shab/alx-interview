ef130b3a8190d32745cd476a9027a4e07ec5da24
Mohammed <mrcloud.med@gmail.com>
2024-07-25 22:42:22 +0000

    fix_prb

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,18 @@
+#!/usr/bin/python3
 def pascal_triangle(n):
+    """Function that return a list of integer"""
+
     if n <= 0:
         return []
-    
     triangle = []
-    
+
     for i in range(n):
-        row = [1] * (i + 1)  # Start each row with 1s
-        
-        # Update values based on the previous row
+        row = [1] * (i + 1)
+
+
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-        
+
         triangle.append(row)
-    
+
     return triangle
