0fcbf4c5aa1b441df751782dfdb3ad8ffe4c21b4
Mohammed <mrcloud.med@gmail.com>
2024-09-06 02:44:33 +0200

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,37 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+// Function to get and display characters of a given movie
+function getCharacters (movieId) {
+  const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+  // Fetch film data
+  request(filmUrl, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      console.error('Error fetching movie details:', error || response.statusCode);
+      return;
+    }
+
+    const filmData = JSON.parse(body);
+    const characterUrls = filmData.characters;
+
+    // Fetch each character's name
+    characterUrls.forEach((url) => {
+      request(url, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
+        }
+      });
+    });
   });
-};
+}
+
+// Check if movie ID is provided as a command line argument
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Please provide a Movie ID as the first argument');
+  process.exit(1);
+}
+
+getCharacters(movieId);
