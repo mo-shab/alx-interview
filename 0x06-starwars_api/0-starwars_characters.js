7467091830d7493055c57e85fbcea9d21947204d
Mohammed <mrcloud.med@gmail.com>
2024-09-06 02:40:23 +0200

    Starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,37 +2,26 @@
 
 const request = require('request');
 
-// Function to get and display characters of a given movie
-function getCharacters (movieId) {
-  const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-  // Fetch film data
-  request(filmUrl, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-      console.error('Error fetching movie details:', error || response.statusCode);
-      return;
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
     }
-
-    const filmData = JSON.parse(body);
-    const characterUrls = filmData.characters;
-
-    // Fetch each character's name
-    characterUrls.forEach((url) => {
-      request(url, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        }
-      });
-    });
   });
-}
+};
 
-// Check if movie ID is provided as a command line argument
-const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Please provide a Movie ID as the first argument');
-  process.exit(1);
-}
-
-getCharacters(movieId);
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);
