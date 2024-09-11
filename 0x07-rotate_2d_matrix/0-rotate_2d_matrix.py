0bf7df375b85f35a6dde92d0aea3f34df1e8fd3f
Mohammed <mrcloud.med@gmail.com>
2024-09-11 20:02:11 +0000

    Fix pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,15 +8,15 @@ def rotate_2d_matrix(matrix):
     """
     Rotate Matrix, return nothing, modify the matrix in-place
     """
-    l, r = 0, len(matrix) -1
+    left, r = 0, len(matrix) - 1
 
-    while l < r:
-        for i in range(r - l):
-            top, bottom = l, r
-            topLeft = matrix[top][l + i]
-            matrix[top][l + i] = matrix[bottom - i][l]
-            matrix[bottom - i][l] = matrix[bottom][r - i]
+    while left < r:
+        for i in range(r - left):
+            top, bottom = left, r
+            topLeft = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][r - i]
             matrix[bottom][r - i] = matrix[top + i][r]
             matrix[top + i][r] = topLeft
         r -= 1
-        l += 1
+        left += 1
